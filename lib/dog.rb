be8e625a553a72a47ab738a5babac5332451299d
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-10-20 10:46:52 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+  def name=(dogs_name)  ##setter method
+    @this_dogs_name = dogs_name
+  end
+  def name  ##getter method
+    @this_dogs_name
+  end
 end
+
+
+rex = Dog.new
+rex.name = "Rex"
+
+puts rex.name 
